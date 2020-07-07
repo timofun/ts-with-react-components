dad4e3fb48a8375fe64480999d5ba6fb63cb6d52
fqy1000 <fqy1000@qq.com>
2020-07-07 17:17:53 +0800

    feat button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Hello from './components/Hello'
-import LikeButton from './components/LikeButton'
+import React from 'react'
+import './App.css'
+import './styles/index.scss'
+import Button, { ButtonSize, ButtonType } from './components/Button/button'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <LikeButton />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button btnType={ButtonType.Default} disabled className="asdasd">Hello</Button>
+        <Button size={ButtonSize.Small}>Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hells</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>Hells</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com">baidu</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>baidu</Button>
       </header>
     </div>
   );
