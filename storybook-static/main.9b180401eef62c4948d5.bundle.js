bd7bdfa73787ec63e92b8e6133c8220760ae4692
fanqingyu <fanqingyu@jc-ai.com>
2020-07-23 09:38:21 +0800

    update .travis.yml

---
+++ storybook-static/main.9b180401eef62c4948d5.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1255:function(n,o,c){"use strict";c.r(o);c(1256),c(1292)},430:function(n,o,c){c(431),c(578),n.exports=c(1255)},495:function(n,o){}},[[430,1,2]]]);
---
+++ storybook-static/precache-manifest.7233fb98daaa07232a3b92eeaeaf1897.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8cfab065c7e0fc17474e5182dd1451d",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.fbfa8bd50cd85b66e045.bundle.js"
+  },
+  {
+    "url": "runtime~main.fbfa8bd50cd85b66e045.bundle.js"
+  },
+  {
+    "revision": "491d76b7022c28670f30",
+    "url": "static/css/main.dc4d4ac8.chunk.css"
+  },
+  {
+    "revision": "49717378a614b96fac4b",
+    "url": "static/css/vendors~main.f8f248a5.chunk.css"
+  },
+  {
+    "url": "vendors~main.fbfa8bd50cd85b66e045.bundle.js"
+  },
+  {
+    "url": "vendors~main.fbfa8bd50cd85b66e045.bundle.js.LICENSE.txt"
+  }
+]);
