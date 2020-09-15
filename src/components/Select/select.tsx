edb22968ff59d16e35b661552065d51e3f9d0283
fanqingyu <timo_quiet@163.com>
2020-09-15 20:25:22 +0800

    build: 新增rc-select库

---
+++ src/components/Select/select.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import classNames from 'classnames'
+const Select: React.FC = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default Select
--- src/typings/typings.d.ts
+++ src/typings/typings.d.ts
@@ -1,3 +1,5 @@
 declare module 'rc-checkbox';
 
 declare module 'rc-util*';
+
+declare module 'rc-select'
